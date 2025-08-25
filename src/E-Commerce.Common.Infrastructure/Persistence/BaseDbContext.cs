2f0a330bc03e5d34375b2ccc7bfc047d4e6e4bee
Heko Weiss <git@smartsolutionslab.tech>
2025-08-25 19:45:03 +0200

    changes v 1.0.2

--- src/E-Commerce.Common.Infrastructure/Persistence/BaseDbContext.cs
+++ src/E-Commerce.Common.Infrastructure/Persistence/BaseDbContext.cs
@@ -1,10 +1,7 @@
 using E_Commerce.Common.Domain.Primitives;
-using E_Commerce.Common.Domain.ValueObjects;
 using E_Commerce.Common.Infrastructure.Services;
-using E_Commerce.Common.Infrastructure.Messaging;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System.Linq.Expressions;
 
 namespace E_Commerce.Common.Infrastructure.Persistence;
 
@@ -12,29 +9,12 @@ public abstract class BaseDbContext : DbContext
 {
     private readonly ITenantService _tenantService;
     private readonly IPublisher _publisher;
-    private readonly IMessageBroker _messageBroker;
 
-    protected BaseDbContext(DbContextOptions options, ITenantService tenantService, IPublisher publisher, IMessageBroker messageBroker) 
+    protected BaseDbContext(DbContextOptions options, ITenantService tenantService, IPublisher publisher)
         : base(options)
     {
         _tenantService = tenantService;
         _publisher = publisher;
-        _messageBroker = messageBroker;
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        // Apply tenant filter to all entities
-        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-        {
-            if (typeof(Entity<>).IsAssignableFromGeneric(entityType.ClrType))
-            {
-                modelBuilder.Entity(entityType.ClrType)
-                    .HasQueryFilter(CreateTenantFilter(entityType.ClrType));
-            }
-        }
-
-        base.OnModelCreating(modelBuilder);
     }
 
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
@@ -47,19 +27,11 @@ public abstract class BaseDbContext : DbContext
 
         var result = await base.SaveChangesAsync(cancellationToken);
 
-        // Publish domain events locally
         foreach (var domainEvent in domainEvents)
         {
             await _publisher.Publish(domainEvent, cancellationToken);
         }
 
-        // Publish integration events to message broker
-        foreach (var domainEvent in domainEvents)
-        {
-            await _messageBroker.PublishDomainEventAsync(domainEvent, cancellationToken);
-        }
-
-        // Clear events after publishing
         ChangeTracker
             .Entries<Entity<object>>()
             .Select(x => x.Entity)
@@ -67,28 +39,5 @@ public abstract class BaseDbContext : DbContext
             .ForEach(entity => entity.ClearDomainEvents());
 
         return result;
-    }
-
-    private LambdaExpression CreateTenantFilter(Type entityType)
-    {
-        var parameter = Expression.Parameter(entityType, "e");
-        var tenantProperty = Expression.Property(parameter, nameof(Entity<object>.TenantId));
-        var tenantValue = Expression.Property(
-            Expression.Constant(_tenantService), 
-            nameof(ITenantService.TenantId));
-        
-        var equal = Expression.Equal(tenantProperty, tenantValue);
-        return Expression.Lambda(equal, parameter);
-    }
-}
-
-public static class TypeExtensions
-{
-    public static bool IsAssignableFromGeneric(this Type genericType, Type type)
-    {
-        return type.GetInterfaces().Any(i => 
-            i.IsGenericType && i.GetGenericTypeDefinition() == genericType) ||
-            (type.BaseType?.IsGenericType == true && 
-             type.BaseType.GetGenericTypeDefinition() == genericType);
     }
 }
